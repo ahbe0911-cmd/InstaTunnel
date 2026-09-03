20e0757ef5330c7b1837d24fc17c6449c18b485e
ahbe0911-cmd <ahbe0911@gmail.com>
2026-09-03 11:18:31 +0330

    Configure native tunnel ABIs and package bridge

---
+++ app/src/main/jni/Application.mk
@@ -0,0 +1,6 @@
+APP_OPTIM := release
+APP_PLATFORM := android-26
+APP_ABI := armeabi-v7a arm64-v8a
+APP_CFLAGS := -O3 -DPKGNAME=hev/htproxy -DCLSNAME=TProxyService
+APP_CPPFLAGS := -O3 -std=c++11
+NDK_TOOLCHAIN_VERSION := clang
